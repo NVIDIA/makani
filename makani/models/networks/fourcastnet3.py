593674d64f7ed06b2133ee9d9ddcd0cbaee6bb18
Thorsten Kurth <tkurth@nvidia.com>
2025-07-21 00:52:03 -0700

    fixing versions

--- makani/models/networks/fourcastnet3.py
+++ makani/models/networks/fourcastnet3.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from dataclasses import dataclass
 import math
 import torch
 import torch.nn as nn
