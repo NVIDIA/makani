0e16f4e6669ff0f7537f001e884514b68b23857f
Ștefan Săftescu <stefan.saftescu@gmail.com>
2026-02-05 20:01:45 +0000

    Fix syntax error in climatology script

--- data_process/generate_wb2_climatology.py
+++ data_process/generate_wb2_climatology.py
@@ -264,7 +264,7 @@ def generate_wb2_climatology(metadata_file: str, input_climatology: str, mask_ou
 
 
 def main(args):
-    generate_wb2_climatology(args.metadata_file, args.input_climatology, args.mask_output_file, args.climatology_output_file, verbose=args.verbose))
+    generate_wb2_climatology(args.metadata_file, args.input_climatology, args.mask_output_file, args.climatology_output_file, verbose=args.verbose)
 
     return
 
