314edccc8bcf2df61467a6d60a226cbadce023db
Thorsten Kurth <tkurth@nvidia.com>
2026-02-18 06:24:34 -0800

    improving stats compute

--- makani/utils/loss.py
+++ makani/utils/loss.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional, Tuple, List
+from typing import Optional, List
 from functools import partial
 import math
 
