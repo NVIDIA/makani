00f53059f8211100c5745a9f6ab7d8c75998f5cd
Thorsten Kurth <tkurth@nvidia.com>
2026-04-21 02:23:40 -0700

    bumping up version number for coszen v2 file

--- makani/third_party/climt/zenith_angle_v2.py
+++ makani/third_party/climt/zenith_angle_v2.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 #
 # This file contains code from the climt project (https://github.com/CliMT/climt)
